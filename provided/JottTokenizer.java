6d71f07da3cbbd16e4af09e734abd4fc9a1aca59
Jiayi Huang <huangjiayi0716@gmail.com>
2026-05-27 22:16:22 -0400

    Reads file, add logic for parsing int, but did not tested

--- provided/JottTokenizer.java
+++ provided/JottTokenizer.java
@@ -6,6 +6,9 @@ package provided;
  * @author 
  **/
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class JottTokenizer {
@@ -17,6 +20,47 @@ public class JottTokenizer {
      * @return an ArrayList of Jott Tokens
      */
     public static ArrayList<Token> tokenize(String filename){
+
+		// reading the file
+		ArrayList<Token> tokens = new ArrayList<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))){
+
+			StringBuilder incompleteToken = new StringBuilder();
+			boolean dotPresent = false;
+			String line;
+			int lineNum = 0;
+
+			while ((line = br.readLine()) != null){
+				lineNum++;
+				for (int i = 0; i < line.length(); i++ ){
+					char ch = line.charAt(i);
+
+					// if char is a digit, append to current ongoing token
+					if (Character.isDigit(ch)){
+						incompleteToken.append(ch);
+					}
+					else if (ch == '.'){
+						// if dot encountered and do is not present
+						// if next char is digit, we append to current ongoing token and set dot to present
+						if ( i+1< line.length() && Character.isDigit(line.charAt(i+1))){
+							incompleteToken.append(ch);
+							dotPresent = true;
+						}
+						else{
+							if (!incompleteToken.isEmpty()){
+								Token newToken = new Token(incompleteToken.toString(), filename, lineNum, TokenType.NUMBER);
+								incompleteToken.setLength(0);
+								dotPresent = false;
+							}
+							incompleteToken.append(ch);
+						}
+					}
+				}
+
+			}
+		} catch (IOException e){
+			System.out.println(e.getMessage());
+		}
 		return null;
 	}
 }
