a25009b5242aabd3581e0e0e6771670bd5b96970
knn4602 <knn4602@rit.edu>
2026-06-18 18:48:35 -0400

    finished fbody node!

--- Nodes/FBodyNode.java
+++ Nodes/FBodyNode.java
@@ -3,21 +3,48 @@ package Nodes;
 import provided.JottTree;
 
 import java.util.ArrayList;
-
+/**
+ * File name: FBodyNode.java
+ * Author: Kifekachukwu Nwosu
+ *
+ * This file defines the FBodyNode class, which represents a grammar for function body
+ * in the Jott parse tree shown the following:
+ *
+ *
+ * The class provides functionality to parse a function body based on the provided grammar.
+ * It also allows conversion to Jott, Java, C and Python
+ */
 public class FBodyNode implements JottTree {
 
     ArrayList<VarDecNode> varDec;
     private ReturnStmtNode returnStmt;
 
-    public FBodyNode(ArrayList<VarDecNode> varDec, ReturnStmtNode returnStmt){
-        this.varDec = varDec;
-        this.returnStmt = returnStmt;
+    private ArrayList<VarDecNode> varDecList;
+    private BodyNode bodyNode;
+
+    public FBodyNode(ArrayList<VarDecNode> varDecList, BodyNode bodyNode) {
+        this.varDecList = varDecList;
+        this.bodyNode = bodyNode;
     }
 
     @Override
     public String convertToJott() {
-        return null;
+
+        String result = "";
+
+        if (varDecList != null) {
+            for (VarDecNode varDec : varDecList) {
+                result += varDec.convertToJott();
+            }
+        }
+
+        if (bodyNode != null) {
+            result += bodyNode.convertToJott();
+        }
+
+        return result;
     }
+
     @Override
     public String convertToJava(String className){
         return null;
