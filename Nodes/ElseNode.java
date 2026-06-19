52f413197f50482f557d4a08bfa3a325bbc81558
Teju Rajbabu <tejurajbabu@gmail.com>
2026-06-19 09:47:22 -0400

    Added If, Elseif, Else, and While nodes wth parsing and jott conversion. Minimal comments

--- Nodes/ElseNode.java
+++ Nodes/ElseNode.java
@@ -1,15 +1,42 @@
 package Nodes;
 
 import provided.JottTree;
+import provided.Token;
+
+import java.util.ArrayList;
+
+/**
+ * File name: ElseNode.java
+ * Author: Teju Rajbabu
+ *
+ * This file defines the ElseNode class, which represents the grammar:
+ *
+ * <else> -> Else {<body>}
+ *
+ * The class provides functionality to parse an else-statement.
+ */
 
 public class ElseNode implements JottTree {
 
-    public ElseNode() {
+    private BodyNode body;
+
+    public ElseNode(BodyNode body) {
+        this.body = body;
+    }
+
+    public static ElseNode parse(ArrayList<Token> tokens) {
+        ParserHelper.expectValue(tokens, "Else", "Expected 'Else'");
+
+        ParserHelper.expectValue(tokens, "{", "Expected '{' after Else");
+        BodyNode body = BodyNode.parse(tokens);
+        ParserHelper.expectValue(tokens, "}", "Expected '}' to close else body");
+
+        return new ElseNode(body);
     }
 
     @Override
     public String convertToJott() {
-        return "";
+        return "Else {" + body.convertToJott() + "}";
     }
 
     @Override
--- Nodes/IfStmtNode.java
+++ Nodes/IfStmtNode.java
@@ -1,15 +1,107 @@
 package Nodes;
 
 import provided.JottTree;
+import provided.Token;
+import provided.JottParser;
+
+import java.util.ArrayList;
+
+/**
+ * File name: IfStmtNode.java
+ * Author: Teju Rajbabu
+ *
+ * This file defines the IfStmtNode class, which represents the grammar:
+ *
+ * <if_stmt> -> If [<expr>] {<body>} <elseif> * <else>
+ *
+ * The class provides functionality to parse an if statement based on the grammar.
+ * It also allows conversion back to Jott.
+ */
 
 public class IfStmtNode implements JottTree {
 
-    public IfStmtNode() {
-    }
+    private ExprNode condition;
+    private BodyNode ifBody;
+    private ArrayList<ElseIfNode> elseIfList;
+    private ElseNode elseNode;
+
+    public IfStmtNode(ExprNode condition, BodyNode ifBody, 
+        ArrayList<ElseIfNode> elseIfList, ElseNode elseNode) {
+            this.condition = condition;
+            this.ifBody = ifBody;
+            this.elseIfList = (elseIfList != null) ? elseIfList : new ArrayList<>();  //this.elseIfList = elseIfList;
+            this.elseNode = elseNode;
+        }
+        public static IfStmtNode parse(ArrayList<Token> tokens) {
+
+            // "If"
+            ParserHelper.expectValue(tokens, "If", "Expected 'If'");
+
+            // "["
+            ParserHelper.expectValue(tokens, "[", "Expected '[' after If");
+
+            // condition
+            ExprNode cond = ExprNode.parse(tokens);
+
+            // "]"
+            ParserHelper.expectValue(tokens, "]", "Expected ']' after condition");
+
+            // "{"
+            ParserHelper.expectValue(tokens, "{", "Expected '{' to start If body");
+
+            // parse body statements
+            ArrayList<BodyStmtNode> stmts = new ArrayList<>();
+            while (BodyStmtNode.startsBodyStmt(tokens)) {
+                stmts.add(BodyStmtNode.parse(tokens));
+            }
+
+            // optional return
+            ReturnStmtNode ret = null;
+            if (tokens.get(0).getToken().equals("Return")) {
+                ret = ReturnStmtNode.parse(tokens);
+            }
+
+            // "}"
+            ParserHelper.expectValue(tokens, "}", "Expected '}' to end If body");
+
+            BodyNode ifBody = new BodyNode(stmts, ret);
+
+            // parse ElseIf blocks
+            ArrayList<ElseIfNode> elseIfs = new ArrayList<>();
+            while (ElseIfNode.startsElseIf(tokens)) {
+                elseIfs.add(ElseIfNode.parse(tokens));
+            }
+
+            // parse optional Else block
+            ElseNode elseNode = null;
+            if (ElseNode.startsElse(tokens)) {
+                elseNode = ElseNode.parse(tokens);
+            }
+
+            return new IfStmtNode(cond, ifBody, elseIfs, elseNode);
+        }
+
+
 
     @Override
     public String convertToJott() {
-        return "";
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("If[");
+        sb.append(condition.convertToJott());
+        sb.append("]{");
+        sb.append(ifBody.convertToJott());
+        sb.append("}");
+
+        for (ElseIfNode e : elseIfList) {
+            sb.append(e.convertToJott());
+        }
+
+        if (elseNode != null) {
+            sb.append(elseNode.convertToJott());
+        }
+
+        return sb.toString();
     }
 
     @Override
